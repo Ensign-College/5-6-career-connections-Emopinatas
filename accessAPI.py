445406a30916221a6cab621774b5e08593b66f2a
Emopinatas <167846406+Emopinatas@users.noreply.github.com>
2024-05-23 09:27:26 +0900

    Update accessAPI.py

--- accessAPI.py
+++ accessAPI.py
@@ -1,4 +1,17 @@
 import requests
 import json
 
-base_url = 'https://openscriptureapi.org/api/scriptures/v1/lds/en/volume/bookofmormon/'
+print("Welcome to the Book of Mormon Summary Tool!")
+while True:
+  book = input("Which book of the Book of Mormon would you like?")
+  chapter = input("Which chapter of {book} are you interested in?")
+
+  base_url = f'https://openscriptureapi.org/api/scriptures/v1/lds/en/volume/bookofmormon/{book}/{chapter}'
+
+  response = requests.get(url)
+  data = response.json()
+  print("Summary of {book} chapter {chapter}:" + data['chapter']['summary'],"\n")
+  if input("Would you like to view another (Y/N)?") != Y:
+    print("Thank you for using the Book of Mormon Summary Tool!")
+    break
+
